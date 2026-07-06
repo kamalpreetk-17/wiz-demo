eb0b03ad401e2c45929224a8cd68527dcc1f6e6c
Kamalpreet Kaur <kamalpreetkaur@Kamalpreets-MacBook-Air-2.local>
2026-07-05 23:28:47 -0400

    Added IRSA for ALB

--- infrastructure/root/alb.tf
+++ infrastructure/root/alb.tf
@@ -1,3 +1,18 @@
+module "load_balancer_controller_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                              = "${module.eks.cluster_name}-alb-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
 # AWS Load Balancer Controller
 resource "helm_release" "aws_alb_controller" {
   name       = "aws-load-balancer-controller"
@@ -7,8 +22,15 @@ resource "helm_release" "aws_alb_controller" {
   values = [<<EOF
 clusterName: ${module.eks.cluster_name}
 vpcId: ${module.vpc.vpc_id}
+serviceAccount:
+  create: true
+  name: aws-load-balancer-controller
+  annotations:
+    "eks.amazonaws.com/role-arn": ${module.load_balancer_controller_irsa_role.iam_role_arn}
 EOF
   ]
 
    depends_on = [module.eks] 
 }
+
+
