fe6e2780498f4b00641b515f2e43e41b5f1e6751
Kamalpreet Kaur <kamalpreetkaur@Kamalpreets-MacBook-Air-2.local>
2026-07-05 20:35:51 -0400

    Updated the eks

--- infrastructure/root/provider.tf
+++ infrastructure/root/provider.tf
@@ -24,7 +24,8 @@ provider "aws" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    # Reverted to cluster_ca_certificate
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
     
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
@@ -33,7 +34,7 @@ provider "helm" {
         "eks", 
         "get-token", 
         "--cluster-name", module.eks.cluster_name,
-        "--region", "us-east-1"  
+        "--region", "us-east-1"
       ]
     }
   }
