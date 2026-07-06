286228c01ff2175c9befcfbdf122f46e1a75dee6
Kamalpreet Kaur <kamalpreetkaur@Kamalpreets-MacBook-Air-2.local>
2026-07-05 23:57:55 -0400

    Commented out the IRSA code

--- infrastructure/root/alb.tf
+++ infrastructure/root/alb.tf
@@ -1,18 +1,18 @@
-module "load_balancer_controller_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
+# module "load_balancer_controller_irsa_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "~> 5.0"
 
-  role_name                              = "${module.eks.cluster_name}-alb-controller"
-  attach_load_balancer_controller_policy = true
+#   role_name                              = "${module.eks.cluster_name}-alb-controller"
+#   attach_load_balancer_controller_policy = true
 
-  oidc_providers = {
-    ex = {
-      # This will now work perfectly because enable_irsa = true
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
+#   oidc_providers = {
+#     ex = {
+#       # This will now work perfectly because enable_irsa = true
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+#     }
+#   }
+# }
 
 # AWS Load Balancer Controller
 resource "helm_release" "aws_alb_controller" {
@@ -23,15 +23,16 @@ resource "helm_release" "aws_alb_controller" {
   values = [<<EOF
 clusterName: ${module.eks.cluster_name}
 vpcId: ${module.vpc.vpc_id}
-serviceAccount:
-  create: true
-  name: aws-load-balancer-controller
-  annotations:
-    "eks.amazonaws.com/role-arn": ${module.load_balancer_controller_irsa_role.iam_role_arn}
 EOF
   ]
 
    depends_on = [module.eks] 
 }
+
+# serviceAccount:
+#   create: true
+#   name: aws-load-balancer-controller
+#   annotations:
+#     "eks.amazonaws.com/role-arn": ${module.load_balancer_controller_irsa_role.iam_role_arn}
 
 
