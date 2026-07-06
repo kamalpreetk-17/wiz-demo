39ba130c272a6518aa1ec8500c9cde70ea8e2c4b
Kamalpreet Kaur <kamalpreetkaur@Kamalpreets-MacBook-Air-2.local>
2026-07-06 15:03:35 -0400

    Updated role

--- infrastructure/root/alb.tf
+++ infrastructure/root/alb.tf
@@ -3,16 +3,20 @@
 #   raw_oidc_url = try(module.eks.cluster_oidc_issuer_url, "")
 # }
 
+data "aws_iam_openid_connect_provider" "eks" {
+  url = module.eks.cluster_oidc_issuer_url
+}
+
 module "load_balancer_controller_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.39.0"
+  version = "~> 5.0"
 
   role_name                              = "${module.eks.cluster_name}-alb-controller"
   attach_load_balancer_controller_policy = true
 
   oidc_providers = {
     main = {
-      provider_arn               = module.eks.oidc_provider_arn
+      provider_arn               = data.aws_iam_openid_connect_provider.eks.arn
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
   }
