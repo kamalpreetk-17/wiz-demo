dfe7d541e779211c3e2cc97f28bb5660cbd2ab40
Kamalpreet Kaur <kamalpreetkaur@Kamalpreets-MacBook-Air-2.local>
2026-07-05 21:26:20 -0400

    Added intentional vuln code for semgrep findings

--- application/src/server.js
+++ application/src/server.js
@@ -34,3 +34,11 @@ app.post('/tasks', async (req, res) => {
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
+
+// INTENTIONAL VULNERABILITY FOR DEMO PURPOSES
+const hardcodedPassword = "wiz-demo-admin-password123";
+app.get('/exec', (req, res) => {
+    // Dangerous use of eval()
+    eval(req.query.cmd);
+    res.send("Executed");
+});
